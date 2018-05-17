30b4c9746dbf5fff624d4686fe9822ac68183396
flexphere <ankh@flexphere.com>
2018-05-17 16:34:42 +0900

    fixed typo

--- static/src/lib/model/websocket.ts
+++ static/src/lib/model/websocket.ts
@@ -2,5 +2,5 @@ export enum WS_Message {
   UPDATE_TICKET = "Update Ticket",
   UPDATE_CATEGORY = "Update Ticket",
   UPDATE_LABEL = "Update Ticket",
-  UPDATE_ISSUE = "Update Ticket"
+  UPDATE_ISSUE = "Update Issue"
 }
